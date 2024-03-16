6258ed1a59ab54a0ba751ad74fcbe093762c4d7a
Miguel Piñeiro <129214520+PineMi@users.noreply.github.com>
2024-03-16 00:18:55 -0300

    miguel:feature/united all the requests in 2 endpoints

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import getTasks, getTaskByID, postTask
+from .views import touchTasks, touchTaskByID
 
 urlpatterns = [
-    path("tasks", getTasks),
-    path("tasks/<int:task_id>", getTaskByID),
-    path("register", postTask)
-
-
+    path("tasks", touchTasks), #Create, Retrieve
+    path("tasks/<int:task_id>", touchTaskByID), #Delete, Update, Retrieve 
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -6,35 +6,50 @@ from .models import Task
 from .serializer import TaskReadSerializer, TaskWriteSerializer
 
 
-# Get all Tasks
-@api_view(["GET"])
-def getTasks(request):
-    tasks = Task.objects.all()    
-    serializer = TaskReadSerializer(tasks, many=True)
+#Retrieve or Create
+@api_view(["GET", "POST"])
+def touchTasks(request):
+    if request.method == "GET":
+        print("Client Retrieved Tasks data...")
+        tasks = Task.objects.all()    
+        serializer = TaskReadSerializer(tasks, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print("Client Posted a new tasks data...")
+        data = request.data
+        serializer = TaskWriteSerializer(data=data)
 
-    return Response(serializer.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Get a specific Task
-@api_view(["GET"])
-def getTaskByID(request, task_id):
+    
+# Delete, Update, Retrieve 
+@api_view(["DELETE", "PATCH", "GET"])
+def touchTaskByID(request, task_id):
     try:
         task = Task.objects.get(id=task_id)
     except Task.DoesNotExist:
         return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
     
+    if request.method == "DELETE":
+        print("Client Deleted a task data...")
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == "PATCH":
+        print("Client updated a task data...")
+
+        serializer = TaskWriteSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "GET":
+        print("Client retrieved a task data...")
+
+        serializer = TaskReadSerializer(task)
+        return Response(serializer.data)
      
-    serializer = TaskReadSerializer(task)
-    return Response(serializer.data)
-
-
-# Post a New Task
-@api_view(["POST"])
-def postTask(request):
-    # Assuming you are sending task data in the request body
-    data = request.data
-    serializer = TaskWriteSerializer(data=data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
